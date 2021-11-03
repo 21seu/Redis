7944f69aebbd81c10df8573c19a91bbfc4413b1c
21seu <1693376841@qq.com>
2021-11-03 23:48:17 +0800

    redis模拟验证码发送案例demo

--- 01_Java_Redis/src/main/java/com/ftj/redis/RedisDemo02.java
+++ 01_Java_Redis/src/main/java/com/ftj/redis/RedisDemo02.java
@@ -1,5 +1,7 @@
 package com.ftj.redis;
 
+import redis.clients.jedis.Jedis;
+
 import java.util.Random;
 
 /**
@@ -12,12 +14,15 @@ import java.util.Random;
 public class RedisDemo02 {
 
     public static void main(String[] args) {
-        System.out.println(getCode());
+        //模拟验证码发送
+        verify("15922881790");
+
     }
 
 
     /**
      * 生成6位验证码
+     *
      * @return
      */
     public static String getCode() {
@@ -32,7 +37,49 @@ public class RedisDemo02 {
     /**
      * 每个手机每天只能发送三次，验证码放到redis中，设置过期时间
      */
-    public static void verify(){
+    public static void verify(String phone) {
+        //连接redis
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        //手机发送次数key
+        String countKey = "VerifyCode" + phone + ":count";
+        //验证码key
+        String codeKey = "VerifyCode" + phone + ":code";
+        //每个手机每天只能发送3次
+        if (jedis.get(countKey) == null) {
+            //没有发送次数，则第一次发送，设置发送次数为1
+            jedis.setex(codeKey, 24 * 60 * 60, "1");
+        } else if (Integer.parseInt(jedis.get(countKey)) <= 2) {
+            //发送次数+1
+            jedis.incr(codeKey);
+        } else if (Integer.parseInt(jedis.get(countKey)) > 2) {
+            System.out.println("今天的放松次数已经超过3次");
+            jedis.close();
+            return;
+        }
 
+        //发送验证码放到redis
+        String vcode = getCode();
+        jedis.setex(codeKey, 120, vcode);
+        jedis.close();
+    }
+
+    /**
+     * 验证码校验
+     *
+     * @param phone
+     * @param code
+     */
+    public static void getRedisCode(String phone, String code) {
+        //连接redis
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        //验证码key
+        String codeKey = "VerifyCode" + phone + ":code";
+        String redisCode = jedis.get(codeKey);
+        if (redisCode.equals(code)) {
+            System.out.println("成功");
+        } else {
+            System.out.println("失败");
+        }
+        jedis.close();
     }
 }
